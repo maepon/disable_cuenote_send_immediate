7ea8b6d9bb8324515a2b0c6b6dafd46fa34074d1
Masayuki Maekawa <maepon@gmail.com>
2024-05-01 17:30:28 +0900

    v.1.0.1

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,6 @@
 // background
 
-const cuenoteRegexp = /^https:\/\/.*asp\.cuenote\.jp\//
+const cuenoteRegexp = /^https:\/\/.*\.cuenote\.jp\//
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && cuenoteRegexp.test(tab.url)) {
